676c01027f6f9d1ee18aff7ebd483551bd1af587
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2026-03-10 10:02:07 +0700

    ud: set warn to label required

--- src/utils/bpmnlint-custom-config.ts
+++ src/utils/bpmnlint-custom-config.ts
@@ -19,7 +19,7 @@ const customRules = {
   "event-based-gateway": "off",
   "event-sub-process-typed-start-event": "off",
   "global": "off",
-  "label-required": "off",
+  "label-required": "warn",
   "link-event": "off",
   "no-bpmndi": "off",
   "no-complex-gateway": "off",
